471a8f3a50deb5dd55dd2fcf2cf20ec96ef1f684
lly <lily@dehuinet.com>
2019-03-06 22:45:17 +0800

    简易的vue

---
+++ even loop/1.js
@@ -0,0 +1,11 @@
+setTimeout(() => console.log(5),0);
+
+new Promise(resolve => {
+    console.log(1);
+    resolve(3)
+    Promise.resolve().then(() => console.log(4))
+}).then(num => {
+    console.log(num)
+})
+
+console.log(2)
