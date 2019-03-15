058419af39d56aef6dc13a940b2f9ca87f2d274e
lly <lily@dehuinet.com>
2019-03-16 03:19:51 +0800

    。

--- 算法/罗马数字.js
+++ 算法/罗马数字.js
@@ -14,25 +14,37 @@ const romanEntity = {
     "CD": 400,
     "CM": 900
 }
-const spStr = (str) => {
-    return ["IV","IX","XL","XC","CD","CM"].reduce((acc,curr) => {
-        const regRes = str.match(new RegExp(`${curr}`));
-        const oneStr = regRes ? regRes.reduce((acc,curr) => {
-                return acc + romanEntity[curr]
-            },0) 
-        : 0 ;
-        return acc+oneStr
-    },0)
-}
+// const spStr = (str) => {
+//     return ["IV","IX","XL","XC","CD","CM"].reduce((acc,curr) => {
+//         const regRes = str.match(new RegExp(`${curr}`));
+//         const oneStr = regRes ? regRes.reduce((acc,curr) => {
+//                 return acc + romanEntity[curr]
+//             },0) 
+//         : 0 ;
+//         return acc+oneStr
+//     },0)
+// }
+
+
+// const romanToInt = (str) => {
+//     const reg = /IV|IX|XL|XC|CD|CM/g;
+//     const str1 = str.replace(reg,'')
+//     const romanArr = str1.split('').reduce((acc,curr) => {
+//         return acc+romanEntity[curr]
+//     },0)
+//     return romanArr + spStr(str)
+// }   
+
+// console.log(romanToInt('IMV'))
 
 
 const romanToInt = (str) => {
-    const reg = /IV|IX|XL|XC|CD|CM/g;
-    const str1 = str.replace(reg,'')
-    const romanArr = str1.split('').reduce((acc,curr) => {
-        return acc+romanEntity[curr]
+    return str.split('').reduce((acc,curr,index,originArr) => {
+        if( index === originArr.length-1)  return acc + romanEntity[curr]
+        return romanEntity[curr] >= romanEntity[originArr[index+1]]
+            ? acc + romanEntity[curr]
+            : acc - romanEntity[curr]
     },0)
-    return romanArr + spStr(str)
-}   
+}  
 
-console.log(romanToInt('IX'))
+console.log(romanToInt('IV'))
