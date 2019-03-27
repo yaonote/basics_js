c51b5967cec10772c18280b974ce3cf5ab8e7b2a
lly <lily@dehuinet.com>
2019-03-27 18:34:36 +0800

    pipe

--- 面试题/2.js
+++ 面试题/2.js
@@ -1,53 +1,72 @@
 // 特定语法匹配替换
 // 说明：匹配字符串中形如 =g文字文字= 的语法，并将相应部分转化为对应的标签文字文字
 // 示例：
-transform('=g1.18 进入开发=');  // <g>1.18 进入开发</g>
-transform('=p1.23 联调(-1)=，=p1.25 发布(+1)=')//  <g>1.23 联调(-1)</g>，<g>1.25 发布(+1)</g>
-transform('1.25 发布') // 1.25 发布
+// transform('=g1.18 进入开发=');  // <g>1.18 进入开发</g>
+// transform('=p1.23 联调(-1)=，=p1.25 发布(+1)=')//  <g>1.23 联调(-1)</g>，<g>1.25 发布(+1)</g>
+// transform('1.25 发布') // 1.25 发布
 
-const transform = (str) => str.replace(/=g|=/g,(s) => {
-    return {
-        '=g': '<g>',
-        '=': '</g>'
-    }[s]
-})
-
-const transform = (str) => {
-    return str.replace(/=([A-z])([a-z])/g,'$1<g>$2').replace(/=/g,'</g>')
-}
-console.log(transform('=Hs1.23 联调(-1)=，=g1.25 发布(+1)='));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// const transform = (str) => str.replace(/=g|=/g,(s) => {
+//     return {
+//         '=g': '<g>',
+//         '=': '</g>'
+//     }[s]
+// })
+// console.log(transform('=g1.23 联调(-1)=，=g1.25 发布(+1)='));
 
 // 合并数组中相邻且重复的元素
 // 说明：请实现一个函数 merge，传入一个数组，合并数组中【相邻且重复】的元素。
 // 示例：
-merge([3,2,2,4,5,5,6,2,1]); // 输出[3,2,4,5,6,2,1]
-merge([3,2,3]); // 输出[3,2,3]
-merge([2,2,3]); // 输出[2,3]
 
+// merge([3,2,2,4,5,5,6,2,1]); // 输出[3,2,4,5,6,2,1]
+// merge([3,2,3]); // 输出[3,2,3]
+// merge([2,2,3]); // 输出[2,3]
+
+// const merge = (arr) => {
+//     return arr.reduce((acc,curr,i,origin) => {
+//         return curr === origin[i+1] ? acc : [...acc,curr]
+//     },[])
+// }
+// console.log(merge([2,2,3,2]));
 
 // 函数组合运行
 // 说明：实现一个方法，可将多个函数方法按从左到右的方式组合运行。
 // 如composeFunctions(fn1,fn2,fn3,fn4)等价于fn4(fn3(fn2(fn1))。 示例：
-const add = x => x + 1;
-const multiply = (x, y) => x * y;
-const multiplyAdd = composeFunctions(multiply, add);
-multiplyAdd(3, 4) // 返回 13
 
+// function composeFunctions () {
+//     const fnList = Array.from(arguments);
+//     return function() {
+//         const args = Array.from(arguments);
+//         let temp,ret;
+//         fnList.forEach((fn,index) => {
+//             if(index === 0){
+//                 temp = fn.apply(null,args)
+//             }else if(index === (fnList.length - 1)){
+//                 ret = fn(temp)
+//             }else{
+//                 temp = fn(temp)
+//             }
+//         })
+//         return ret
+//     }
+// } 
+
+// function composeFunctions (...funcs) {
+//     if(funcs.length === 0){
+//         return arg => arg
+//     }
+//     if(funcs.length === 1){
+//         return funcs[0]
+//     }
+//     return funcs.reduce((acc,curr) => (...args) => curr(acc(...args)))
+// }
+
+// const add = x => x + 1;
+// const multiply = (x, y) => x * y;
+// const xx = x => x + 10;
+// const multiplyAdd = composeFunctions(multiply, add, xx);
+// // multiplyAdd(3, 4) // 返回 13
+
+// console.log(multiplyAdd(3, 4))
 
 // 第二个问题是Promise的用法以及实现原理。
 // 第三个问题是前端存储方式，以及它们之间的优缺点。
--- 面试题/tempCodeRunnerFile.js
+++ 面试题/tempCodeRunnerFile.js
@@ -1,5 +1,17 @@
-const protectPhone = (phone) => {
-    return phone.split('').splice(3,4,'****').join('')
+function composeFunctions (...funcs) {
+    if(funcs.length === 0){
+        return arg => arg
+    }
+    if(funcs.length === 1){
+        return funcs[0]
+    }
+    return funcs.reduce((acc,curr) => (...args) => curr(acc(...args)))
 }
 
-console.log(protectPhone('18512341234'))
+const add = x => x + 1;
+const multiply = (x, y) => x * y;
+const xx = x => x + 10;
+const multiplyAdd = composeFunctions(multiply, add, xx);
+// multiplyAdd(3, 4) // 返回 13
+
+console.log(multiplyAdd(3, 4))
